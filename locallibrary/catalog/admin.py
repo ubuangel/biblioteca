06818f50ea99326a58860bb2658b6623bdaf11be
angel <andresbjar97@gmail.com>
2020-11-27 21:30:35 -0500

    agregar registro al administrador y crear super usuario -------- luego plantilla base

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Author,Book,Genre,BookInstance
+#La aplicación de administración de Django puede usar tus modelos para construir automáticamente un área dentro del sitio que 
+#puedes usar para crear, consultar, actualizar y borrar registros. 
 
+#toda la cnofiguracion para agregar para incluir la aplciacion addmin en tu sitio web 
+#fue echa en el esqueleto del projecto
+#Registra los modelos copiando el texto siguiente al final del archivo.
 # Register your models here.
+
+#Como resultado, todo lo que  debes 
+#hacer para agregar tus modelos a la aplicación admin  es  registrarlos
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+
+#crear super usuario reinicir servidor
