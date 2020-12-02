7b410415f2b2e6299e8fc348d41a07a1bfeb3e9e
angel <andresbjar97@gmail.com>
2020-12-01 22:39:18 -0500

    incluyendo modulos y renderizar

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 
 urlpatterns = [
- url(r'^$', views.index, name='index'),
+ url(r'^$', views.index, name='index'),# view.index una funcion llamada index() en views.py#funcion vista que sera llamda si el patron es detectado
 ]
